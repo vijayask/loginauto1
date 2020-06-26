7991f8c07532b6cb34e318ee645b0a2c6f8bb5e3
vijayask <65651668+vijayask@users.noreply.github.com>
2020-06-26 20:50:03 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,27 +2,56 @@ from selenium import webdriver
 import time
 import unittest
 import HtmlTestRunner
-class LoginTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Chrome(executable_path="C:/Users/VIJAYA/Desktop/webdrivers/chromedriver_win32/chromedriver")
-        cls.driver.implicitly_wait(10)
-        cls.driver.maximize_window()
 
-    def test_login_valid(self):
-        self.driver.get("http://datamd-md-india.qa.i3systems.in/")
-        self.driver.find_element_by_id("id_username").send_keys("admin")
-        self.driver.find_element_by_id("id_password").send_keys("!3$y$+eMs")
-        self.driver.find_element_by_class_name("btn").click()
-        time.sleep(2)
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.close(cls)
-        cls.driver.quit()
-        print("Test Completed")
-        
+from pomproject.pages.loginpage import LoginPage
+from pomproject.pages.homepage import HomePage
+
+
+
+class LoginTest(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+      cls.driver = webdriver.Chrome(executable_path="C:/Users/VIJAYA/Desktop/webdrivers/chromedriver_win32/chromedriver")
+      cls.driver.implicitly_wait(5)
+      cls.driver.maximize_window()
+
+  def test_login_valid(self):
+      driver = self.driver
+      driver.get("http://datamd-md-india.qa.i3systems.in/")
+      login = LoginPage(driver)
+      login.enter_username("admin")
+      login.enter_password("!3$y$+eMs")
+      login.click_login()
+
+  @classmethod
+  def test_logout_valid(self):
+      driver = self.driver
+          #self.driver.find_element_by_id("logoutCaret").click()
+          #self.driver.find_element_by_link_text("Logout").click()
+
+      homepage = HomePage(driver)
+      homepage.click_downarrow()
+      homepage.click_logout()
+      #self.driver.find_element_by_id("id_username").send_keys("admin")
+      #self.driver.find_element_by_id("id_password").send_keys("!3$y$+eMs")
+      #self.driver.find_element_by_class_name("btn").click()
+      time.sleep(10)
+  @classmethod
+  def tearDownClass(cls):
+      cls.driver.close()
+      cls.driver.quit()
+      print("Test Completed")
+
 if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/VIJAYA/PycharmProjects/seleniump/lotr/pyproj/pomp/lloottss/reports'))
+   unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/VIJAYA/PycharmProjects/seleniump/pomproject/reports'))
+
+
+
+
+
+
+
 
 
 
