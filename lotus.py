0499d655aadd8cdcc8406afd115c0ff665981541
vijayask <65651668+vijayask@users.noreply.github.com>
2020-06-26 12:25:28 +0530

    Add files via upload

---
+++ lotus.py
@@ -0,0 +1,12 @@
+class Locators():
+
+    #login page objects
+    username_textbox_id = "id_username"
+    password_textbox_id = "id_password"
+    login_button_id = "btn"
+
+    #home page objects
+    downarrow_link_id = "logoutCaret"
+    logout_link_linkText = "Logout"
+
+    
