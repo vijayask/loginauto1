867783de698ca13bb0f1de93e5db9683b270d5c1
vijayask <65651668+vijayask@users.noreply.github.com>
2020-06-26 12:23:37 +0530

    Add files via upload

---
+++ loginpage.py
@@ -0,0 +1,23 @@
+from lotr.pyproj.pomp.lloottss.lotus import Locators
+class LoginPage():
+
+    def __init__(self,driver):
+        self.driver = driver
+
+
+        self.username_textbox_id = Locators.username_textbox_id
+        self.password_textbox_id = "id_password"
+        self.login_button_class_name = "btn"
+
+    def enter_username(self,username):
+        self.driver.find_element_by_id(Locators.username_textbox_id).clear()
+        self.driver.find_element_by_id(self.username_textbox_id).send_keys("admin")
+
+    def enter_password(self,password):
+        self.driver.find_element_by_id(self.password_textbox_id).clear()
+        self.driver.find_element_by_id(self.password_textbox_id).send_keys("!3$y$+eMs")
+
+    def click_login(self):
+        self.driver.find_element_by_class_name(self.login_button_class_name).click()
+
+
